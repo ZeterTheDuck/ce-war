16a2cf06d09b8181c56925e799950e15078c19eb
ZeterTheDuck <zeter.madsen@gmail.com>
2026-05-08 14:52:14 -0600

    docs: add documentation for CardReader

--- src/main/java/com/cewar/util/CardReader.java
+++ src/main/java/com/cewar/util/CardReader.java
@@ -9,12 +9,18 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * 
+ * Class for {@link #readCards()}
  */
 public class CardReader {
     // File source of all cards, represented as JSON
     public static final String CARD_INDEX = "ce-war\\src\\main\\resources\\private\\cardIndex.json";
 
+    /**
+     * Takes the JSON file associated with the card data, and converts it into a list of {@link Card}s.
+     * 
+     * @return List of {@link Card}s
+     * @throws IOException
+     */
     public static List<Card> readCards() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         List<Card> listCard = objectMapper.readValue(new File(CARD_INDEX), new TypeReference<List<Card>>(){});
