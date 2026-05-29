dc69ac579be7ca752156ff4a751dbd789c3946e7
ZeterTheDuck <zeter.madsen@gmail.com>
2026-05-28 18:17:26 -0600

    docs: improve documentation for GameCard

--- src/main/java/com/cewar/model/session/GameCard.java
+++ src/main/java/com/cewar/model/session/GameCard.java
@@ -27,7 +27,9 @@ public class GameCard implements Comparable<GameCard> {
     private final Card refCard;
 
     /**
-     * This card's location on the field. If this card is attached to another card, this variable shouldn't matter.
+     * This card's location on the field.
+     * 
+     * <p> If this card is attached to another card, this variable shouldn't matter.
      */
     @Getter @Setter
     private Location location;
@@ -46,7 +48,7 @@ public class GameCard implements Comparable<GameCard> {
      * <ul><li> X-position of 1 is player 1's draw slots, Y-position indicates index
      * <li> X-position of 2 is player 2's draw slots, Y-position indicates index.</ul>
      * 
-     * <p>
+     * <p> 
      * REVIEW Usage of X-position to indicate owner is likely not necessary, there are not
      * currently any cards or combination of cards that could end up in the opponent's side field
      * or deck.
@@ -71,7 +73,7 @@ public class GameCard implements Comparable<GameCard> {
     private int attack, health, maxHealth;
 
     /**
-     * The card that this card is attached to
+     * The card that this card is attached to.
      * 
      * <p> If this value is null, then this card is the top card in a stack, or not on the field
      * 
@@ -80,11 +82,17 @@ public class GameCard implements Comparable<GameCard> {
     @Getter @Setter
     private GameCard attachedTo;
 
+    /**
+     * Unordered map of all cards attached to this card. 
+     * 
+     * <p> Cards are mapped by their {@link #ucid}
+     */
     @Getter @Setter
     private HashMap<Long, GameCard> attachedCards;
 
     /**
-     * Whether this card should display as upside-down. Used in-game to signify that the owner is different.
+     * Whether this card should display as upside-down. Used in-game to signify that the owner is
+     * different.
      * 
      * <p> A value of false indicates that this card is owned by its original owner, 
      * whereas a value of true indicates that this card is owned by the opposing player.
@@ -93,15 +101,24 @@ public class GameCard implements Comparable<GameCard> {
     private boolean rotated;
 
     /**
-     * Whether this card should be visible to the opponent. Used primarily for the player's hand and action slots
+     * Whether this card should be visible to the opponent. Used primarily for the player's hand
+     * and action slots.
      * 
      * <p> This is not secure, as the opponent can check the JSON data of game updates.
      * 
-     * <p> Cards in the (main) deck are always invisible, and cards in the GY and Dump are always visible.
+     * <p> Cards in the (main) deck are always invisible, and cards in the GY and Dump are always
+     * visible.
      */
     @Getter @Setter
     private boolean visible;
 
+    /**
+     * Map of counters attached to this card. 
+     * 
+     * <p> The key of an entry is the name of the counter, i.e. "Time"
+     * 
+     * <p> The value of an entry is the number of counters for that key
+     */
     @Getter
     private HashMap<String, Integer> counters;
 
@@ -117,8 +134,7 @@ public class GameCard implements Comparable<GameCard> {
     }
 
     /**
-     * Sets the corresponding number of counters attached
-     * to this card
+     * Sets the corresponding number of counters attached to this card
      * 
      * @param counter - name of counter
      * @param amount - amount to set
@@ -138,7 +154,8 @@ public class GameCard implements Comparable<GameCard> {
     /**
      * Recursively finds the topmost card that this card is stacked to.
      * 
-     * @return the topmost card that this card is stacked to, or this card if it is the topmost in the stack
+     * @return the topmost card that this card is stacked to, or this card if it is the topmost in
+     * the stack
      */
     public GameCard getStackTop() {
         if (attachedTo == null) {
@@ -150,7 +167,9 @@ public class GameCard implements Comparable<GameCard> {
 
     /**
      * Attach this card to another card, recursively repeating for cards attached to this card.
-     * <p> Resets counter, health, attack, etc., and changes location and position to match other card.
+     * 
+     * <p> Resets counter, health, attack, etc., and changes location and position to match other
+     * card.
      * 
      * @param other Card to attach this card to
      */
@@ -160,7 +179,7 @@ public class GameCard implements Comparable<GameCard> {
             card.attachTo(other);
         }
 
-        // 2. Clear data=
+        // 2. Clear data
         resetData();
 
         // 3. Attach to other card
