e3ede68a5da8cd9055a3a449dfdae3bcf67e3020
ZeterTheDuck <zeter.madsen@gmail.com>
2026-05-29 20:32:16 -0600

    feat: add getById operations to UserService for UserCards and UserDecks

--- src/main/java/com/cewar/services/UserService.java
+++ src/main/java/com/cewar/services/UserService.java
@@ -8,23 +8,36 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.cewar.model.entity.User;
+import com.cewar.model.entity.UserCard;
+import com.cewar.model.entity.UserDeck;
+import com.cewar.repositories.UserCardRepository;
+import com.cewar.repositories.UserDeckRepository;
 import com.cewar.repositories.UserRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
 /**
- * Service to access and manage Users.
+ * Service to access and manage {@link com.cewar.model.entity.User Users}, as well as 
+ * {@link com.cewar.model.entity.UserCard UserCards} and {@link com.cewar.model.entity.UserDeck UserDecks}
  */
 @Service
 public class UserService implements org.springframework.security.core.userdetails.UserDetailsService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+
+    @Autowired
+    private UserCardRepository cardRepo;
+
+    @Autowired
+    private UserDeckRepository deckRepo;
+
+    /* SECTION User Repository Methods */
 
     /* Required for DaoAuthenticationProvider to work, would just be "getUserByUsername" if possible */
     @Override
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> result = repo.findByUsernameIgnoreCase(username);
+        Optional<User> result = userRepo.findByUsernameIgnoreCase(username);
         if (result.isEmpty()) {
             throw new UsernameNotFoundException("Username not found with username: " + username);
         }
@@ -36,7 +49,7 @@ public class UserService implements org.springframework.security.core.userdetail
     }
 
     public User getByUsernameIgnoreCase(String username) throws UsernameNotFoundException {
-        Optional<User> result = repo.findByUsernameIgnoreCase(username);
+        Optional<User> result = userRepo.findByUsernameIgnoreCase(username);
         if (result.isEmpty()) {
             throw new UsernameNotFoundException("Username not found with username when ignoring case: " + username);
         }
@@ -44,7 +57,7 @@ public class UserService implements org.springframework.security.core.userdetail
     }
 
     public User getByEmail(String email) throws EntityNotFoundException {
-        Optional<User> result = repo.findByEmail(email);
+        Optional<User> result = userRepo.findByEmail(email);
         if (result.isEmpty()) {
             throw new EntityNotFoundException("User not found with email " + email);
         }
@@ -54,7 +67,7 @@ public class UserService implements org.springframework.security.core.userdetail
     /* SECTION CRUD Operations from CrudRepository */
 
     public User getById(long id) throws EntityNotFoundException {
-        Optional<User> result = repo.findById(id);
+        Optional<User> result = userRepo.findById(id);
         if (result.isEmpty()) {
             throw new EntityNotFoundException("User not found with ID " + id);
         }
@@ -62,19 +75,44 @@ public class UserService implements org.springframework.security.core.userdetail
     }
 
     public Collection<User> getAll() {
-        return repo.findAll();
+        return userRepo.findAll();
     }
 
     public User save(User user) {
-        return repo.save(user);
+        return userRepo.save(user);
     }
 
     public Collection<User> saveAll(Iterable<User> users) {
-        return repo.saveAll(users);
+        return userRepo.saveAll(users);
     }
 
     public void delete(User user) {
-        repo.delete(user);
+        userRepo.delete(user);
+    }
+
+    /* !SECTION */
+    /* !SECTION */
+
+    /* SECTION UserCard Repository Methods */
+
+    public UserCard getCardById(long id) throws EntityNotFoundException {
+        Optional<UserCard> result = cardRepo.findById(id);
+        if (result.isEmpty()) {
+            throw new EntityNotFoundException("Deck not found with ID " + id);
+        }
+        return result.get();
+    }
+
+    /* !SECTION */
+
+    /* SECTION UserDeck Repository Methods */
+
+    public UserDeck getDeckById(long id) throws EntityNotFoundException {
+        Optional<UserDeck> result = deckRepo.findById(id);
+        if (result.isEmpty()) {
+            throw new EntityNotFoundException("Deck not found with ID " + id);
+        }
+        return result.get();
     }
 
     /* !SECTION */
